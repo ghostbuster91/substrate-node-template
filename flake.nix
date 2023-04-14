21a61b69bf3229ceccb6ee6b11ac5f901738f382
Kasper Kondzielski <kasper.kondzielski@iohk.io>
2023-04-14 10:41:29 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -19,6 +19,14 @@
 
             BINDGEN_EXTRA_CLANG_ARGS = with pkgs;
               "-isystem ${libclang.lib}/lib/clang/${lib.getVersion libclang}/include";
+            shellHook = ''
+              rustup install 1.68.0
+              rustup default 1.68.0
+
+              rustup toolchain install nightly-2023-01-30
+              rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-30
+              rustup override set nightly-2023-01-30
+            '';
           };
       });
 }
