73b2aad8c8c1db7f39fd0d10796fe389105157a6
Kasper Kondzielski <kasper.kondzielski@iohk.io>
2023-04-14 09:30:46 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -13,13 +13,10 @@
       {
         devShells.default =
           pkgs.mkShell {
-            packages = with pkgs; [ protobuf llvm rust ];
-            # buildInputs = with pkgs; [ libclang ];
+            packages = [ pkgs.protobuf pkgs.llvm pkgs.rustup pkgs.rustc ];
+            buildInputs = with pkgs; [ libclang ];
             LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            HOST_CXXFLAGS =
-              if isDarwin
-              then "-I ${pkgs.darwin.apple_sdk.CLTools_Executables}/usr/include/c++/v1 -I ${pkgs.darwin.apple_sdk.CLTools_Executables}/usr/include"
-              else "";
+
             BINDGEN_EXTRA_CLANG_ARGS = with pkgs;
               "-isystem ${libclang.lib}/lib/clang/${lib.getVersion libclang}/include";
           };
